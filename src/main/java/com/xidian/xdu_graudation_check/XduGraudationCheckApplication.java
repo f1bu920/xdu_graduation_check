bb26641571b87b8a36bacdded12db5867af85c7e
f1bu920 <2377478740@qq.com>
2021-12-29 18:16:28 +0800

    first commit

---
+++ src/main/java/com/xidian/xdu_graudation_check/XduGraudationCheckApplication.java
@@ -0,0 +1,13 @@
+package com.xidian.xdu_graudation_check;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XduGraudationCheckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XduGraudationCheckApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xidian/xdu_graudation_check/XduGraudationCheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xidian.xdu_graudation_check;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XduGraudationCheckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
